76a028f5d59cee62a4616ed31d6bed92eb2ebb4f
sixtean <leandroalves.webdeveloper@gmail.com>
2025-04-28 23:44:49 -0300

    Banco Atualizado 2

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,10 +6,14 @@ dotenv.config();
 
 export const Connection = mysql.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: 46876
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000
 });
 
 
